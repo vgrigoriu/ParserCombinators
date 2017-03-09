7f18819beca68821731a1fa16042a2a87bd73252
Victor Grigoriu <vgrigoriu@gmail.com>
2017-03-09 10:47:19 +0200

    Add project files.

---
+++ ParserCombinators/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ParserCombinators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
