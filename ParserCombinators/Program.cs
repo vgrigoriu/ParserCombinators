78948efcd900609358e2fee3399993ebb0b1109b
Victor Grigoriu <vgrigoriu@gmail.com>
2017-03-22 14:57:28 +0200

    Combining two parsers in sequence

--- ParserCombinators/Program.cs
+++ ParserCombinators/Program.cs
@@ -1,29 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ParserCombinators
 {
-    public delegate Result<(char, string)> Parser(string str);
+    public delegate Result<(T, string)> Parser<T>(string str);
 
     internal static class Program
     {
         private static void Main()
         {
-            Parser aParser = Parse.Char('A');
+            Parser<char> aParser = Parse.Char('A');
             Console.WriteLine(aParser("ABC"));
             Console.WriteLine(aParser("ZBC"));
             Console.WriteLine(aParser(string.Empty));
+
+            Parser<char> bParser = Parse.Char('B');
+            var abParser = Parse.AndThen(aParser, bParser);
+            Console.WriteLine(abParser("ABC"));
+            Console.WriteLine(abParser("ZBC"));
+            Console.WriteLine(abParser("AZC"));
         }
     }
 
     public static class Parse
     {
-        public static Parser Char(char toMatch)
+        public static Parser<char> Char(char toMatch)
         {
             return str =>
             {
                 if (string.IsNullOrEmpty(str))
                 {
-                    return Failure("No more input");
+                    return Failure<char>("No more input");
                 }
 
                 var first = str[0];
@@ -32,13 +39,45 @@ namespace ParserCombinators
                     return Success.Of((toMatch, str.Substring(1)));
                 }
 
-                return Failure($"Expecting '{toMatch}'. Got '{first}'.");
+                return Failure<char>($"Expecting '{toMatch}'. Got '{first}'.");
             };
         }
 
-        private static Failure<(char, string)> Failure(string message)
+        public static Parser<IEnumerable<T>> AndThen<T>(Parser<T> parser1, Parser<T> parser2)
         {
-            return new Failure<(char, string)>(message);
+            return str =>
+            {
+                switch (parser1(str))
+                {
+                    case Failure<(T, string)> f:
+                        return Failure<IEnumerable<T>>(f.Message);
+                    case Success<(T, string)> s1:
+                        (T value1, string remaining1) = s1.Value;
+                        switch (parser2(remaining1))
+                        {
+                            case Failure<(T, string)> f:
+                                return Failure<IEnumerable<T>>(f.Message);
+                            case Success<(T, string)> s2:
+                                (var value2, var remaining2) = s2.Value;
+                                return Success.Of((Pair(value1, value2), remaining2));
+                        }
+
+                        break;
+                }
+
+                return Failure<IEnumerable<T>>("switch was not exhaustive");
+            };
+        }
+
+        private static IEnumerable<T> Pair<T>(T t1, T t2)
+        {
+            yield return t1;
+            yield return t2;
+        }
+
+        private static Failure<(T, string)> Failure<T>(string message)
+        {
+            return new Failure<(T, string)>(message);
         }
     }
 
